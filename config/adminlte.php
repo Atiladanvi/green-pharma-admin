000aa42714f1dedfdcb5f008388372867112d15e
Atiladanvi <atila.danvi@outlook.com>
2021-01-21 22:10:23 -0300

    Add home view and config some stuffs

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => env('APP_NAME', 'APP_NAME'),
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => env('APP_NAME', 'APP_NAME'),
+    'logo_img' => 'img/brand_logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => env('APP_NAME', 'APP_NAME'),
 
     /*
     |--------------------------------------------------------------------------
