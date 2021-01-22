524a7ff619c0566da15f7a0272792d4a337ca23c
Atiladanvi <atila.danvi@outlook.com>
2021-01-21 21:57:39 -0300

    Add initial admin dashboard with Laravel-AdminLTE

---
+++ resources/views/panel.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Panel')
+
+@section('content_header')
+    <h1>Painel</h1>
+@stop
+
+@section('content')
+
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
